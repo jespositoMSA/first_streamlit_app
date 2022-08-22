d523b932161fe4e31d556387160d47649ffb5ee0
jespositoMSA <99439428+jespositoMSA@users.noreply.github.com>
2022-08-22 10:30:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.subtitle('Breakfast Menu')
+streamlit.header('My Parents New Healthy Diner')
+
+streamlit.subheader('Breakfast Menu')
